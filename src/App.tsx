18e2a60a5955be6d5127454fe358a6d8f841249c
Ana Clara Garcia Farah <anaclaragf@outlook.com>
2023-04-04 11:02:17 -0300

    initial config

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Profile } from "./pages";
+import { GlobalStyles } from "./styles/globalStyles";
+
+function App() {
+  return (
+    <>
+      <GlobalStyles />
+      <Profile />
+    </>
+  );
+}
+
+export default App;
